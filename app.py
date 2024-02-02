79fc7df9330f241d114a477db38ca1eb64592023
ddrilling <daytondrilling@gmail.com>
2024-02-02 00:15:06 -0600

    need to implement returnResult

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ class App(ctk.CTk):
         self.title('Crypto Price Search')
         self.geometry(f"{600}x{700}")
 
-        self.grid_rowconfigure(3, weight = 0)
+        self.grid_rowconfigure(4, weight = 0)
         self.grid_columnconfigure(3, weight = 1)
 
         #ids label
@@ -18,12 +18,23 @@ class App(ctk.CTk):
         self.idsLabel.grid(row = 2, column = 1, padx = 20, pady = 20, sticky = "nsew")
 
         #ids entry 
-        self.idsEntry = ctk.CTkEntry(self, placeholder_text = 'ethereum')
+        self.idsEntry = ctk.CTkEntry(self, placeholder_text = 'Enter the ids of desired curency')
         self.idsEntry.grid(row = 2, column = 2, columnspan = 2, padx = 20, pady = 20, sticky = "nsew")
 
         #search ids button
         self.idsSearchButton = ctk.CTkButton(self, fg_color = 'black',text = 'Search')
         self.idsSearchButton.grid(row = 3, column = 2, columnspan = 2, rowspan = 1, padx = 10, pady = 20, sticky = "ew")
+
+        #get value from entry
+        ids = self.idsEntry.get()
+
+        #field for price of currency
+        self.displayBox = ctk.CTkTextbox(self, width = 100, height = 50)
+        self.displayBox.grid(row = 4, column = 2, columnspan = 2, padx = 20, pady = 20, sticky = "nsew")
+
+    def returnResult(self):
+        
+
 
 if __name__ == '__main__':
     app = App()
