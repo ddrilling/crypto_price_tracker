4700ffa3cf0338d0c1748fc8eff8c5847be6bebd
ddrilling <daytondrilling@gmail.com>
2024-02-04 18:10:29 -0600

    need to add trending boxes

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-##
+### Crypto Price Search :mag:
+
+This is a personal project to learn and experiment with the customtkinter library, and learn more about crypto apis.
+
+---
+
+### Description :truck:
+
+I intend to work on this project until it is something I like:
+
+- :art: A much better UI
+- :chart_with_upwards_trend: Keep track of selected coins' prices and add charts
+- :memo: Crypto news sidebar
+- :page_facing_up: Turn into an executable
+- :chart_with_upwards_trend: Eventually create a chart analysis tool for stocks and crypto
--- app.py
+++ app.py
@@ -9,29 +9,39 @@ class App(ctk.CTk):
         super().__init__()
 
         self.title('Crypto Price Search')
-        self.geometry(f"{600}x{700}")
+        self.geometry(f"{1100}x{580}")
 
+        #grid layout
+        self.grid_columnconfigure(1,weight = 1)
         self.grid_rowconfigure(4, weight = 0)
         self.grid_columnconfigure(3, weight = 1)
 
+        #sidebar frame 
+        self.sideBarFrame = ctk.CTkFrame(self, width = 250)
+        self.sideBarFrame.grid(row = 0, column = 0, rowspan = 2, sticky = 'nsew')
+        self.sideBarFrame.grid_rowconfigure(5, weight = 1)
+        self.sideBarTitle = ctk.CTkLabel(self.sideBarFrame, text = 'Trending', font = ctk.CTkFont(size = 16, weight = 'bold'), width = 250)
+        self.sideBarTitle.grid(row = 0, column = 0, padx = 10, pady = (10, 20))
+
         #ids label
-        self.idsLabel = ctk.CTkLabel(self, text = 'IDS')
-        self.idsLabel.grid(row = 2, column = 1, padx = 20, pady = 20, sticky = 'nsew')
+        self.idsLabel = ctk.CTkLabel(self, text = 'Search by ID')
+        self.idsLabel.grid(row = 1, column = 1, padx = 50, pady = 20, sticky = 'nsew')
 
         #ids entry 
         self.idsEntry = ctk.CTkEntry(self, placeholder_text = 'Enter the ids of desired curency')
-        self.idsEntry.grid(row = 2, column = 2, columnspan = 2, padx = 20, pady = 20, sticky = 'nsew')
+        self.idsEntry.grid(row = 2, column = 1, columnspan = 2, padx = 50, pady = 20, sticky = 'nsew')
 
         #search ids button
         self.idsSearchButton = ctk.CTkButton(self, fg_color = 'black',text = 'Search', command = self.fillDisplayBox)
-        self.idsSearchButton.grid(row = 3, column = 2, columnspan = 2, rowspan = 1, padx = 10, pady = 20, sticky = 'ew')
+        self.idsSearchButton.grid(row = 3, column = 1, columnspan = 2, rowspan = 1, padx = 50, pady = 20, sticky = 'ew')
 
         #get value from entry
         ids = self.idsEntry.get()
 
         #field for price of currency
         self.displayBox = ctk.CTkTextbox(self, width = 100, height = 50)
-        self.displayBox.grid(row = 4, column = 2, columnspan = 2, padx = 20, pady = 20, sticky = 'nsew')
+        self.displayBox.grid(row = 4, column = 1, columnspan = 2, padx = 50, pady = 20, sticky = 'nsew')
+
 
     #pass ids through price_tracker
     def getIds(self):
